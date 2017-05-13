5630877ae756db645c1215f9cd1703d17b7a4b32
Elias Kotlyar <elias.kotlyar@mediawave.de>
2017-05-14 00:01:45 +0200

    Points

---
+++ quicksolver/Point.js
@@ -0,0 +1,10 @@
+function Point(x, y){
+    this.x = x || 0;
+    this.y = y || 0;
+}
+Point.prototype.x = null;
+Point.prototype.y = null;
+Point.prototype.addPath = function(pointArray){
+
+};
+global.Point = Point;
