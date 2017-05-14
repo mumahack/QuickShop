ef2bf1bfa5f935fb8bc9316c05b1aeb09a37a67e
Elias Kotlyar <elias.kotlyar@mediawave.de>
2017-05-14 02:50:46 +0200

    Hash Stuff

--- quicksolver/Point.js
+++ quicksolver/Point.js
@@ -9,22 +9,28 @@ Point.prototype.targetPaths = null;
 Point.prototype.debug = function(prefixString){
     console.log(prefixString+ this.x +":" +this.y);
 }
+
+Point.prototype.getHash = function(){
+    return this.x +":" +this.y;
+}
 Point.prototype.addPath = function(targetPoint,pathPoint){
-    this.targetPaths[targetPoint] = this.targetPaths[targetPoint] || [];
-    console.log("Len:"+this.targetPaths[targetPoint].length);
+    var hash = targetPoint.getHash();
+    this.targetPaths[hash] = this.targetPaths[hash] || [];
+    console.log("Len:"+this.targetPaths[hash].length);
     //this.debug("Current Path: " );
     //targetPoint.debug ("TargetPoint: ");
     //pathPoint.debug ("Path:Point ");
     //console.log(pathPoint);
 
-    this.targetPaths[targetPoint].push(pathPoint);
+    this.targetPaths[hash].push(pathPoint);
 };
 
 Point.prototype.getPathLen = function(targetPoint){
-    this.targetPaths[targetPoint] = this.targetPaths[targetPoint] || [];
-    console.log("Len GetPath:"+this.targetPaths[targetPoint].length + " TargetPoint"+targetPoint.x+":"+targetPoint.y);
+    var hash = targetPoint.getHash();
+    this.targetPaths[hash] = this.targetPaths[hash] || [];
+    console.log("Len GetPath:"+this.targetPaths[hash].length + " TargetPoint"+targetPoint.x+":"+targetPoint.y);
     console.log(this.targetPaths);
-    return this.targetPaths[targetPoint].length;
+    return this.targetPaths[hash].length;
     /*
     console.log(this.targetPaths[targetPoint]);
     this.targetPaths[targetPoint] = this.targetPaths[targetPoint] || [];
